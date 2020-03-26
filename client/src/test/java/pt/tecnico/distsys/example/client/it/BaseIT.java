c86103fdbac0e892162edcbd660a11c5c8cf6a2b
Miguel Pardal <miguel.pardal@tecnico.ulisboa.pt>
2020-03-26 15:26:09 +0000

    Add integration tests

---
+++ client/src/test/java/pt/tecnico/distsys/example/client/it/BaseIT.java
@@ -0,0 +1,48 @@
+package pt.tecnico.distsys.example.client.it;
+
+import java.io.IOException;
+import java.util.Properties;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+
+import pt.tecnico.distsys.example.SetNameRequest;
+import pt.tecnico.distsys.example.client.ExampleFrontend;
+
+/**
+ * Base class for integration tests. Extending classes can access test
+ * properties defined in the configuration resource.
+ */
+public class BaseIT {
+	private static final String TEST_PROP_FILE = "/test.properties";
+	protected static Properties properties;
+	static ExampleFrontend frontend;
+
+	@BeforeAll
+	public static void oneTimeSetup() throws IOException {
+		properties = new Properties();
+
+		try {
+			properties.load(BaseIT.class.getResourceAsStream(TEST_PROP_FILE));
+			System.out.println("Test properties:");
+			System.out.println(properties);
+
+		} catch (IOException e) {
+			final String msg = String.format("Could not load properties file %s", TEST_PROP_FILE);
+			System.out.println(msg);
+			throw e;
+		}
+
+		final String host = properties.getProperty("server.host");
+		final int port = Integer.parseInt(properties.getProperty("server.port"));
+		frontend = new ExampleFrontend(host, port);
+	}
+
+	@AfterAll
+	public static void cleanup() {
+	}
+
+	protected SetNameRequest buildRequest(String name) {
+		return SetNameRequest.newBuilder().setProposedName(name).build();
+	}
+}
---
+++ client/src/test/java/pt/tecnico/distsys/example/client/it/SetNameIT.java
@@ -0,0 +1,45 @@
+package pt.tecnico.distsys.example.client.it;
+
+import static io.grpc.Status.Code.INVALID_ARGUMENT;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import io.grpc.StatusRuntimeException;
+
+/** Test suite for setName remote operation. */
+public class SetNameIT extends BaseIT {
+	private static final String EMPTY_ID = "";
+	private static final String INVALID_ID = "   ";
+	private static final String VALID_ID = "friend";
+
+	@BeforeEach
+	public void setUp() {
+	}
+
+	@Test
+	public void validIdTest() {
+		assertEquals(VALID_ID, frontend.setName(buildRequest(VALID_ID)).getName());
+	}
+
+	@Test
+	public void emptyIdTest() {
+		assertEquals(INVALID_ARGUMENT,
+				assertThrows(StatusRuntimeException.class, () -> frontend.setName(buildRequest(EMPTY_ID))).getStatus()
+						.getCode());
+	}
+
+	@Test
+	public void invalidIdTest() {
+		assertEquals(INVALID_ARGUMENT,
+				assertThrows(StatusRuntimeException.class, () -> frontend.setName(buildRequest(INVALID_ID))).getStatus()
+						.getCode());
+	}
+
+	@AfterEach
+	public void clear() {
+	}
+}
